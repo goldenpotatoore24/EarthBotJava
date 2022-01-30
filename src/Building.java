73a79045ff460643e3e20ef3fb74e26d4a9a8ded
ishaandw <spamrobux6@gmail.com>
2022-01-30 00:51:34 -0800

    amongus sussy balls NOAM CAPITALZIE MAIN

--- src/Building.java
+++ src/Building.java
@@ -2,11 +2,47 @@ public class Building {
     private Good producedGood;
     private double efficiency;
     private int workers;
+    private Province location;
 
-    public Building(Good producedGood, double efficiency, int workers)
+    public Building(Good producedGood, double efficiency, int workers, Province location)
     {
         this.producedGood = producedGood;
         this.efficiency = efficiency;
         this.workers = workers;
+        this.location = location;
+    }
+
+
+
+    public Good getProducedGood() {
+        return producedGood;
+    }
+
+    public void setProducedGood(Good producedGood) {
+        this.producedGood = producedGood;
+    }
+
+    public double getEfficiency() {
+        return efficiency;
+    }
+
+    public void setEfficiency(double efficiency) {
+        this.efficiency = efficiency;
+    }
+
+    public int getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(int workers) {
+        this.workers = workers;
+    }
+
+    public Province getLocation() {
+        return location;
+    }
+
+    public void setLocation(Province location) {
+        this.location = location;
     }
 }
--- src/Good.java
+++ src/Good.java
@@ -1,2 +1,51 @@
 public class Good {
+    private String type;
+    private double value;
+    private double supply;
+    private double demand;
+
+    public Good(String type, double value, double supply, double demand)
+    {
+        this.type = type;
+        this.value = value;
+        this.supply = supply;
+        this.demand = demand;
+    }
+
+    public void calculateNewValue()
+    {
+        //TODO: Find a way to calculate a new value from supply and demand
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getSupply() {
+        return supply;
+    }
+
+    public void setSupply(double supply) {
+        this.supply = supply;
+    }
+
+    public double getDemand() {
+        return demand;
+    }
+
+    public void setDemand(double demand) {
+        this.demand = demand;
+    }
 }
