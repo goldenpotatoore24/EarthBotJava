445353ca33e29e10f41e6fea122ad12b4f7a4d9a
goldenpotatoore24 <noamvardi90@gmail.com>
2022-01-30 10:35:40 -0800

    testing intellij commit

--- src/main.java
+++ src/main.java
@@ -1,28 +1,44 @@
 public class main {
 
-
-    private Province[] provinces = new Province[0];
+    private static main realMain = new main();
+    private  Nation[] nations = new Nation[0];
 
     public static void main(String[] args) {
 
-        Province province1 = new Province( 50.0, "Mountain View");
 
+        Nation nation1 = new Nation(realMain, "United States of America");
+        Province province1 = new Province(nation1, 50.0, "Mountain View");
+        Province province2 = new Province(nation1, 30, "Redwood City");
+        Province province3 = new Province(nation1, 0, "San Mateo");
 
         Pop pop1 = new Pop(50, province1, 150, 140);
-        Pop pop2 = new Pop(30, province1, 1000, 500);
+        Pop pop2 = new Pop(30, province2, 1000, 500);
 
-        for(Pop pop : province1.getPops())
-        {
-            System.out.println(pop);
+        for(Nation nation : realMain.nations) {
+
+            for(Province province : nation.getProvinces())
+            {
+                for (Pop pop : province.getPops()) {
+                System.out.println(pop);
+            }
+            }
+
         }
 
     }
 
-    public Province[] provinceArrayPush(Province[] provs, Province input)
+
+
+
+    public  void nationArrayPush(Nation input)
     {
-        Province[] newArr = new Province[provs.length + 1];
+        Nation[] newArr = new Nation[nations.length + 1];
+        for(int i = 0; i < nations.length; i++)
+        {
+            newArr[i] = nations[i];
+        }
         newArr[newArr.length - 1] = input;
-        return newArr;
+        nations = newArr;
     }
 
 
