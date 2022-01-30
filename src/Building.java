c44c4acea40d39821967cda544741f4b3dd447ac
ishaandw <spamrobux6@gmail.com>
2022-01-29 23:33:56 -0800

    Added Building constructor and Building variables

--- src/Building.java
+++ src/Building.java
@@ -1,5 +1,12 @@
 public class Building {
     private Good producedGood;
     private double efficiency;
+    private int workers;
 
+    public Building(Good producedGood, double efficiency, int workers)
+    {
+        this.producedGood = producedGood;
+        this.efficiency = efficiency;
+        this.workers = workers;
+    }
 }
